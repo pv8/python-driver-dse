809b4353a370f93e62baa24b4e227d84ef6a3e54
Adam Holmberg <Adam.Holmberg@datastax.com>
2016-01-25 12:49:12 -0600

    graph module unit tests

---
+++ tests/unit/test_graph.py
@@ -0,0 +1,141 @@
+try:
+    import unittest2 as unittest
+except ImportError:
+    import unittest  # noqa
+
+import json
+
+from dse.graph import SimpleGraphStatement, GraphOptions, Result, _graph_options
+
+
+class GraphResultTests(unittest.TestCase):
+
+    _values = (None, 1, 1.2, True, False, [1, 2, 3], {u'x': 1, u'y': 2})
+
+    def test_result_value(self):
+        for v in self._values:
+            result = self._make_result(v)
+            self.assertEqual(result.value, v)
+
+    def test_result_attr(self):
+        # value is not a dict
+        result = self._make_result(123)
+        with self.assertRaises(ValueError):
+            result.something
+
+        expected = {'a': 1, 'b': 2}
+        result = self._make_result(expected)
+        self.assertEqual(result.a, 1)
+        self.assertEqual(result.b, 2)
+        with self.assertRaises(AttributeError):
+            result.not_present
+
+    def test_result_item(self):
+        # value is not a dict, list
+        result = self._make_result(123)
+        with self.assertRaises(ValueError):
+            result['something']
+        with self.assertRaises(ValueError):
+            result[0]
+
+        # dict key access
+        expected = {'a': 1, 'b': 2}
+        result = self._make_result(expected)
+        self.assertEqual(result['a'], 1)
+        self.assertEqual(result['b'], 2)
+        with self.assertRaises(KeyError):
+            result['not_present']
+        with self.assertRaises(ValueError):
+            result[0]
+
+        # list index access
+        expected = [0, 1]
+        result = self._make_result(expected)
+        self.assertEqual(result[0], 0)
+        self.assertEqual(result[1], 1)
+        with self.assertRaises(IndexError):
+            result[2]
+        with self.assertRaises(ValueError):
+            result['something']
+
+    def test_str(self):
+        for v in self._values:
+            self.assertEqual(str(self._make_result(v)), str(v))
+
+    def test_repr(self):
+        for v in self._values:
+            result = self._make_result(v)
+            self.assertEqual(eval(repr(result)), result)
+
+    def _make_result(self, value):
+        # result is always json-encoded map with 'result' item
+        return Result(json.dumps({'result': value}))
+
+
+class GraphOptionTests(unittest.TestCase):
+
+    opt_mapping = {t[0]: t[2] for t in _graph_options}
+
+    api_params = {p: str(i) for i, p in enumerate(opt_mapping)}
+
+    def test_init(self):
+        opts = GraphOptions(**self.api_params)
+        self._verify_api_params(opts, self.api_params)
+        self._verify_api_params(GraphOptions(), {})
+
+    def test_update(self):
+        opts = GraphOptions(**self.api_params)
+        new_params = {k: str(int(v) + 1) for k, v in self.api_params.items()}
+        opts.update(GraphOptions(**new_params))
+        self._verify_api_params(opts, new_params)
+
+    def test_get_options(self):
+        # nothing set --> base map
+        base = GraphOptions(**self.api_params)
+        self.assertIs(GraphOptions().get_options_map(base), base._graph_options)
+
+        # something set overrides
+        other = GraphOptions(graph_name='unit_test')
+        options = other.get_options_map(base)
+        updated = self.opt_mapping['graph_name']
+        self.assertEqual(options[updated], 'unit_test')
+        for name in (n for n in self.opt_mapping.values() if n != updated):
+            self.assertEqual(options[name], base._graph_options[name])
+
+        # base unchanged
+        self._verify_api_params(base, self.api_params)
+
+    def test_set_attr(self):
+        expected = 'test@@@@'
+        opts = GraphOptions(graph_name=expected)
+        self.assertEqual(opts.graph_name, expected)
+        expected = 'somethingelse####'
+        opts.graph_name = expected
+        self.assertEqual(opts.graph_name, expected)
+
+        # will update options with set value
+        another = GraphOptions()
+        self.assertIsNone(another.graph_name)
+        another.update(opts)
+        self.assertEqual(another.graph_name, expected)
+
+        opts.graph_name = None
+        self.assertIsNone(opts.graph_name)
+        # will not update another with its set-->unset value
+        another.update(opts)
+        self.assertEqual(another.graph_name, expected)  # remains unset
+        opt_map = opts.get_options_map(another)
+        self.assertIs(opt_map, another._graph_options)
+
+    def test_del_attr(self):
+        opts = GraphOptions(**self.api_params)
+        test_params = self.api_params.copy()
+        del test_params['graph_rebinding']
+        del opts.graph_rebinding
+        self._verify_api_params(opts, test_params)
+
+    def _verify_api_params(self, opts, api_params):
+        self.assertEqual(len(opts._graph_options), len(api_params))
+        for name, value in api_params.items():
+            self.assertEqual(getattr(opts, name), value)
+            self.assertEqual(opts._graph_options[self.opt_mapping[name]], value)
