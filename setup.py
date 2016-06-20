2b09d1c83e490976a3d18cfa68ef70639f0987a9
Adam Holmberg <Adam.Holmberg@datastax.com>
2016-06-20 11:54:54 -0500

    make setup script use documentor

--- setup.py
+++ setup.py
@@ -15,7 +15,9 @@ ez_setup.use_setuptools()
 
 from setuptools import setup
 from distutils.cmd import Command
+from distutils.spawn import find_executable
 import os
+import shutil
 
 long_description = ""
 with open("README.rst") as f:
@@ -34,12 +36,21 @@ class DocCommand(Command):
     def finalize_options(self):
         pass
 
+    def _get_output_dir(self):
+        with open('docs.yaml') as f:
+            for line in f:
+                if line.startswith('output'):
+                    return line.split()[1]
+
     def run(self):
-        path = "docs/_build/%s" % __version__
-        mode = "html"
+
+        if not find_executable('documentor'):
+            raise RuntimeError("'documentor' command not found in path")
+
+        path = self._get_output_dir()
 
         try:
-            os.makedirs(path)
+            shutil.rmtree(path)
         except:
             pass
 
@@ -47,15 +58,15 @@ class DocCommand(Command):
         import subprocess
         try:
             output = subprocess.check_output(
-                ["sphinx-build", "-b", mode, "docs", path],
+                ["documentor", "."],
                 stderr=subprocess.STDOUT)
         except subprocess.CalledProcessError as exc:
-            raise RuntimeError("Documentation step '%s' failed: %s: %s" % (mode, exc, exc.output))
+            raise RuntimeError("Documentation step failed: %s: %s" % (exc, exc.output))
         else:
             print(output)
 
         print("")
-        print("Documentation step '%s' performed, results here:" % mode)
+        print("Documentation step performed, results here:")
         print("   file://%s/%s/index.html" % (os.path.dirname(os.path.realpath(__file__)), path))
 
 # not officially supported, but included for flexibility in test environments
