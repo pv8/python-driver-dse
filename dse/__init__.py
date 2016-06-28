f54943088f2c376a8fbd7ca65534f47fb5fc5c56
Adam Holmberg <Adam.Holmberg@datastax.com>
2016-06-28 13:20:31 -0500

    correct target driver version

--- dse/__init__.py
+++ dse/__init__.py
@@ -14,8 +14,8 @@ from cassandra import NullHandler
 
 logging.getLogger('dse').addHandler(NullHandler())
 
-__version_info__ = (1, 0, 0, 'post0')
+__version_info__ = (1, 0, 1)
 __version__ = '.'.join(map(str, __version_info__))
 
-_core_driver_target_version = '3.5.1'
+_core_driver_target_version = '3.5.0'
 _use_any_core_driver_version = bool(os.environ.get('DSE_DRIVER_PERMIT_UNSUPPORTED_CORE'))
